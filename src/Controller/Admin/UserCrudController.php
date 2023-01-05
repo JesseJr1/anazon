027bc89341098b61ac71078dba26e4e50aab94e0
JesseJr1 <tobias.jess@gmail.com>
2023-01-05 15:40:31 +0100

    Départ apres rebuild et upD avec timeStapable

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        yield EmailField::new('email');
+        yield Field::new('firstname');
+        yield Field::new('plainPassword')
+            ->setHelp('..')
+            ->onlyOnForms();
+    }
+    
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->plainPassword = 'demo';
+        // $user->setPassword('$2y$13$oITgY562c1jP9EGudtMtpucjZSEswSH2ORDVxKFOeop28ZT37wZsm'); // demo
+        $manager->persist($user);
+        
+        $manager->flush();
+    }
+}
